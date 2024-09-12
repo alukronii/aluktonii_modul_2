64534128edeb54cccbe0fd3b9b3ba7090f619bcd
alukronii <ssokiran@yahoo.com>
2024-09-12 21:37:00 +0200

    Done!

--- src/homework/homework2/unload_luggage/Runner.java
+++ src/homework/homework2/unload_luggage/Runner.java
@@ -7,21 +7,29 @@ import java.util.Scanner;
 
 public class Runner {
     public static void main(String[] args) throws IOException {
-        try(InputStream luggage = Runner.class.getClassLoader().getResourceAsStream("luggage.csv"); Scanner scanner = new Scanner(luggage)) {
+        try(InputStream luggage = Runner.class.getClassLoader().getResourceAsStream("luggage.csv");
+            Scanner scanner = new Scanner(luggage)) {
             LinkedList<String> luggageLine = new LinkedList<>();
             int maxCapacity = 10;
-            System.out.println("Начинается загрузка на ленту");
             while (scanner.hasNextLine()) {
-                String[] element = scanner.nextLine().split(";");
-                luggageLine.addLast(element[0]);
-                if (luggageLine.size() == maxCapacity) {
-                    System.out.println("Лента загружена, начинается выдача багажа");
-                    while (!luggageLine.isEmpty()) {
-                        System.out.println(luggageLine.pollFirst() + " - выдан");
-                    }
-                    System.out.println("Лента пустая, закончена выдача багажа");
+                System.out.println("Начинается загрузка на ленту");
+                for (int i = 0; i < maxCapacity; i++) {
+                   if (scanner.hasNextLine()) {
+                        String[] element = scanner.nextLine().split(";");
+                        if (!element[0].equals("номер багажа")) {
+                            luggageLine.addLast(element[0]);
+                        } else {
+                            break;
+                        }
+                   }
                 }
+                System.out.println("Лента загружена, начинается выдача багажа");
+                while (!luggageLine.isEmpty()) {
+                    System.out.println(luggageLine.pollFirst() + " - выдан");
+                }
+                System.out.println("Лента пустая, закончена выдача багажа");
             }
         }
     }
 }
+
